77bf329c9f020ec4e08a021a05fb8cb5507ed5a7
Auto Logger <autologger@example.com>
2025-11-06 18:20:53 +0530

    fix(ClaimDetail): allow page/body scrolling by removing flex-1 from main content wrapper

--- src/components/ClaimDetail.tsx
+++ src/components/ClaimDetail.tsx
@@ -152,7 +152,7 @@ export function ClaimDetail({ claim, onBack }: ClaimDetailProps) {
       </div>
 
       {/* Main Content Area */}
-      <div className="flex-1 flex">
+      <div className="flex">
         {/* Left Side - Tabs Content */}
         <div className="flex-1">
           <Tabs defaultValue="claim-form" className="p-6">
